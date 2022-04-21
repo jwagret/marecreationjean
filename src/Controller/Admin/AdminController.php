d617d3d727769d7f8e491214a9239e7c8453123d
jwagret <jwagret@outlook.fr>
2022-04-21 16:26:44 +0200

    repository methode count

--- src/Controller/Admin/AdminController.php
+++ src/Controller/Admin/AdminController.php
@@ -6,6 +6,7 @@ use App\Repository\CategoriesRepository;
 use App\Repository\ClientsRepository;
 use App\Repository\ProduitsRepository;
 use App\Repository\SousCategoriesRepository;
+use App\Repository\TransporteursRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -17,17 +18,19 @@ class AdminController extends AbstractController
     private ClientsRepository $clientsRepository;
     private CategoriesRepository $categoriesRepository;
     private SousCategoriesRepository $sousCategoriesRepository;
+    private TransporteursRepository $transporteursRepository;
 
     public function __construct(ProduitsRepository $produitsRepository,
                                 ClientsRepository $clientsRepository,
                                 CategoriesRepository $categoriesRepository,
-                                SousCategoriesRepository $sousCategoriesRepository )
+                                SousCategoriesRepository $sousCategoriesRepository,
+                                TransporteursRepository $transporteursRepository)
     {
         $this->produitsRepository = $produitsRepository;
         $this->clientsRepository = $clientsRepository;
         $this->categoriesRepository = $categoriesRepository;
         $this->sousCategoriesRepository = $sousCategoriesRepository;
-
+        $this->transporteursRepository = $transporteursRepository;
     }
 
 
@@ -38,12 +41,14 @@ class AdminController extends AbstractController
         $totalClients = $this->clientsRepository->totalClients();
         $totalCategories = $this->categoriesRepository->totalCategories();
         $totalSousCategories = $this->sousCategoriesRepository->totalSousCategories();
+        $totalTransporteur = $this->transporteursRepository->totalTransporteur();
 
         return $this->render('admin/admin/admin.html.twig', [
             'total_produits' => $totalProduits,
             'total_clients' => $totalClients,
             'total_categories' => $totalCategories,
-            'total_sousCategories' => $totalSousCategories
+            'total_sousCategories' => $totalSousCategories,
+            'total_transporteur' => $totalTransporteur
         ]);
     }
 
